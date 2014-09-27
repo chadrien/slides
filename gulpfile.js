d129b2b482f5245c83ff3e1010631749ddaab1ff
chadrien <chadrien@chadrien.fr>
2014-09-27 13:33:26 +0200

    Removed lot of useless stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     g = require('gulp-load-plugins')(),
-    bowerFiles = require('main-bower-files'),
-    es = require('event-stream');
+    bowerFiles = require('main-bower-files');
 
 var appSrc = [
     './app/**/*',
@@ -42,40 +41,11 @@ gulp.task('tmp-vendors', function () {
 });
 
 function dist(toFolder) {
-    var baseUrl = getBaseUrl();
-
     return gulp.src(appSrc)
-        .pipe(gulp.dest(toFolder))
-        .pipe(es.wait(function () {
-            gulp.src(toFolder + '/**/*.html')
-                .pipe(g.replace(/\{\{BASE_URL\}\}/g, baseUrl))
-                .pipe(gulp.dest(toFolder));
-        }));
+        .pipe(gulp.dest(toFolder));
 }
 
 function vendorDist(toFolder) {
-    var baseUrl = getBaseUrl();
-
     return gulp.src(bowerFiles({includeDev: true}))
-            .pipe(gulp.dest(toFolder))
-            .pipe(es.wait(function () {
-                gulp.src(toFolder + '/screen.css')
-                    .pipe(g.replace(/\.\.\/images/g, baseUrl + '/vendors'))
-                    .pipe(g.replace(/\.\.\/fonts/g, baseUrl + '/vendors'))
-                    .pipe(gulp.dest(toFolder));
-            }));
-}
-
-function getBaseUrl() {
-    var argv = require('minimist')(process.argv.slice(2));
-    var baseUrl = 'http://localhost:3000';
-
-    if (argv['base-url']) {
-        baseUrl = argv['base-url'];
-        if (baseUrl.substr(-1, 1) === '/') {
-            baseUrl = baseUrl.substr(0, baseUrl.length - 1);
-        }
-    }
-
-    return baseUrl;
+        .pipe(gulp.dest(toFolder));
 }
