33287704dd646c0d953f64e4cf0692bf7160ac23
chadrien <chadrien@chadrien.fr>
2014-09-29 23:02:21 +0200

    Much more better Dokerfiles for magento

--- app/2014/docker-par-l-exemple/demo/magento/docker/php/bootstrap.sh
+++ app/2014/docker-par-l-exemple/demo/magento/docker/php/bootstrap.sh
@@ -5,11 +5,20 @@ sleep 5
 magentoRootDir=/usr/share/nginx/html
 localXml="${magentoRootDir}/app/etc/local.xml"
 
-if test -f $localXml; then
-  rm $localXml
+if test -f ${localXml}; then
+  rm ${localXml}
 fi
 
-/usr/bin/n98-magerun --root-dir=$magentoRootDir local-config:generate $MAGENTO_DB_1_PORT_3306_TCP_ADDR root root magento files admin
-/usr/bin/n98-magerun --root-dir=$magentoRootDir db:create
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} local-config:generate ${MAGENTO_DB_1_PORT_3306_TCP_ADDR} root root magento files admin
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} db:create
+
+portLinkedTo80=$(docker ps -a | grep magento_web_1 | grep -o -P "[0-9]+\->80" | tr "\->" "\n" | head -n 1)
+baseUrl="http://localhost:${portLinkedTo80}/"
+
+cd ${magentoRootDir}
+php -d max_execution_time=0 index.php
+
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/unsecure/base_url ${baseUrl}
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/secure/base_url ${baseUrl}
 
 exec $@
--- app/2014/docker-par-l-exemple/demo/magento/docker/web/bootstrap.sh
+++ app/2014/docker-par-l-exemple/demo/magento/docker/web/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sed -i "s/{{FPM_IP}}/$MAGENTO_PHP_1_PORT_9000_TCP_ADDR/" /etc/nginx/sites-available/default
-sed -i "s/{{FPM_PORT}}/$MAGENTO_PHP_1_PORT_9000_TCP_PORT/" /etc/nginx/sites-available/default
+sed -i "s/fastcgi_pass .*:.*;/fastcgi_pass $MAGENTO_PHP_1_PORT_9000_TCP_ADDR:$MAGENTO_PHP_1_PORT_9000_TCP_PORT;/" /etc/nginx/sites-available/default
 
 exec $@
